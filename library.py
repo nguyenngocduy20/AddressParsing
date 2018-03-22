55417c27f52f35d807184e29cd9593f54ac2c1e6
Nguyen Ngoc Duy <nguyenngoc.duy20@gmail.com>
2018-03-22 14:36:58 +0700

    AddressParsing

---
+++ library.py
@@ -0,0 +1,28 @@
+import string
+import re
+import unicodedata
+import sys
+
+patterns = {
+    '[àáảãạăắằẵặẳâầấậẫẩ]': 'a',
+    '[đ]': 'd',
+    '[èéẻẽẹêềếểễệ]': 'e',
+    '[ìíỉĩị]': 'i',
+    '[òóỏõọôồốổỗộơờớởỡợ]': 'o',
+    '[ùúủũụưừứửữự]': 'u',
+    '[ỳýỷỹỵ]': 'y'
+}
+
+
+def remove_accent(text):
+    """
+    Convert from 'Tieng Viet co dau' thanh 'Tieng Viet khong dau'
+    text: input string to be converted
+    Return: string converted
+    """
+    output = text
+    for regex, replace in patterns.items():
+        output = re.sub(regex, replace, output)
+        # deal with upper case
+        output = re.sub(regex.upper(), replace.upper(), output)
+    return output
